48edd6c5136a4be5fb59aecd856126ea6a40222e
Rodrigo <guaxi.prog@gmail.com>
2024-10-13 17:50:15 -0300

    creating Character class

---
+++ GIManager/src/Character.java
@@ -0,0 +1,144 @@
+import util.CheckItemInArray;
+import util.CharacterConstants;
+
+public class Character {
+
+    CheckItemInArray check = new CheckItemInArray();
+
+    private String name;
+    private String vision;
+    private String weapon;
+    private String rarity;
+    private String constellationsLevel;
+    private String meetDate;
+
+    /**
+     * Constructs a Character object with the parameters given.
+     * @param name character's name.
+     * @param vision vision that character wields.
+     * @param weapon weapon that character wields.
+     * @param rarity character's rarity.
+     * @param constellationsLevel character's constellations level.
+     * @param meetDate character's obtaining date.
+     */
+    public Character(String name, String vision, String weapon, String rarity, String constellationsLevel, String meetDate){
+        this.name = name;
+        this.vision = vision;
+        this.weapon = weapon;
+        this.rarity = rarity;
+        this.constellationsLevel = constellationsLevel;
+        this.meetDate = meetDate;
+    }
+
+    /**
+     * Set character's name value.
+     * @param name character's (new) name.
+     */
+    public void setName(String name){
+        this.name = name;
+    }
+
+    /**
+     * Set character's vision value.
+     * @param vision (new) vision that character wields.
+     */
+    public void setVision(String vision){
+        boolean inArray = check.inArray(CharacterConstants.VISIONS, vision);
+        if (inArray){
+            this.vision = vision;
+        }
+        else{System.out.println("Invalid Vision!");}
+    }
+
+    /**
+     * Set character's weapon value.
+     * @param weapon (new) weapon that character wields.
+     */
+    public void setWeapon(String weapon){
+        boolean inArray = check.inArray(CharacterConstants.WEAPONS, weapon);
+        if (inArray){
+            this.weapon = weapon;
+        }
+        else{System.out.println("invalid Weapon!");}
+    }
+
+    /**
+     * Set character's rarity value.
+     * @param rarity character's (new) rarity.
+     */
+    public void setRarity(String rarity){
+        boolean inArray = check.inArray(CharacterConstants.RARIIES, rarity);
+        if (inArray){
+            this.rarity = rarity;
+        }
+        else{System.out.println("Invalid Rarity!");}
+    }
+
+    /**
+     * Set character's constellations level.
+     * @param constellations character's (new) constellations levels.
+     */
+    public void setConstellationsLevel(String constellationsLevel){
+        boolean inArray = check.inArray(CharacterConstants.CONSTELLATIONS_LEVELS, constellationsLevel);
+        if (inArray){
+            this.constellationsLevel = constellationsLevel;
+        }
+        else{System.out.println("Invalid Constellations Level!");}
+    }
+
+    /**
+     * Set character's obtaining date.
+     * @param meetDate character's (new) obtaining date.
+     */
+    public void setMeetDate(String meetDate){
+        this.meetDate = meetDate;
+    }
+
+    /**
+     * Return character's name value.
+     * @return character's name.
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * Return character's vision value.
+     * @return character's vision.
+     */
+    public String getVision(){
+        return this.vision;
+    }
+
+    /**
+     * Return character's weapon value.
+     * @return character's weapon.
+     */
+    public String getWeapon(){
+        return this.weapon;
+    }
+
+    /**
+     * Return character's rarity value.
+     * @return character's rarity.
+     */
+    public String getRarity(){
+        return this.rarity;
+    }
+
+    /**
+     * Return character's constellations level value.
+     * @return character's constellations level.
+     */
+    public String getConstellationsLevel(){
+        return this.constellationsLevel;
+    }
+
+    /**
+     * Return character's obtaining date.
+     * @return character's obatining date.
+     */
+    public String getMeetDate(){
+        return this.meetDate;
+    }
+}
---
+++ GIManager/src/User.java
@@ -0,0 +1,11 @@
+public class User {
+
+    public static void main(String[] args) {       
+        Character char1 = new Character(null, null, null, null, null, null);
+
+        char1.setVision("Pyro");
+        char1.setWeapon("Sword");
+        char1.setRarity("4-STAR");
+        char1.setConstellationsLevel("C6");
+    }
+}
